64d06a5cb31afbdd5cfc0bf630e83d01b261642b
Bi221 <79443322+Bi221@users.noreply.github.com>
2021-02-27 23:03:11 +0530

    Add files via upload

---
+++ assigment cookinfood using go routine and channel.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+    "fmt"
+    "time"
+)
+
+func main() {
+    started:= time now()
+    work=[] string {"comlet wall","complet tiles","wall painting," "wiring electric wire")
+    result:=make(channel bool)
+    <- results
+    work:= range work{
+        go funca(stering){
+            results <- true
+        }(f)fmt.print f(" done in %V \n," time.since (started))
+    }
+    fmt.print f ("working %s....\n," work)
+    time.sleep(2 times. second)
+    fmt.print("done working %s\n,"work)
+    fmt.println(" ")
+    
+    }
