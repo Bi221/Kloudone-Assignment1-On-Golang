8c0038c06de65642b1081e8c114d76a723cc74c7
Bi221 <79443322+Bi221@users.noreply.github.com>
2021-02-27 23:06:34 +0530

    Add files via upload

---
+++ ping pong assignment.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for running race := 1; true; i++ {
+		time.Sleep(time.Second 2)
+		fmt.Println(running source := rand.NewSource(time.Now().Unix())
+		r := rand.New(source)
+		fmt.Print(r.Intn(100), " ", "ping")
+		time.Sleep(time.Second 2)
+		fmt.Println(i, " ", "pong")
+
+	}
+}
