634443839a895ccf60f1ea099e78d7cd443050b1
Bi221 <79443322+Bi221@users.noreply.github.com>
2021-02-27 23:07:07 +0530

    Add files via upload

---
+++ ssever using channel.go
@@ -0,0 +1,17 @@
+package main
+
+import"fmt"
+
+func main((chan int, size)){
+	c <- somevalue *4
+}
+
+func main(){
+	bijeodd:- make(chan int, size)
+	go bije(bijeodd,4)
+	go bije(bijeodd,2)
+	var1:=<-bijeodd
+	var2:=<-bijeodd
+	print(v1,v2)
+
+}
