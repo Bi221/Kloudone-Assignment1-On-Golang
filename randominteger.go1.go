064422ab8ff542b9442c0de4218cda5abbe4d556
Bi221 <79443322+Bi221@users.noreply.github.com>
2021-03-01 21:14:39 +0530

    Add files via upload

---
+++ randominteger.go1.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	("fmt")
+	("math")
+	("time")
+
+)
+func num (i int, a chan int ){
+
+			rand.balls (time.now().unix)}
+			num:= rand int(23,a)
+			c <- mum
+
+func main(){
+	chan:=make( a int)
+	go num (23,a)
+	z:=,<-c	
+			
+			fmt.Println(num)
+
+}
